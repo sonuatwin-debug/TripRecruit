d966d85d874fb48ebc86c9a6fc999840c439d885
win sonuat <sonuatwin@gmail.com>
2025-09-19 05:47:29 +0000

    这是已经好了吗，那现在是需要做什么

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import type { Analytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id",
-  measurementId: "your-measurement-id"
+  apiKey: "AIzaSyB7IW11tIuX1rk1o8KZjv4UmUTawrfQKB4",
+  authDomain: "triprecruit.firebaseapp.com",
+  projectId: "triprecruit",
+  storageBucket: "triprecruit.firebasestorage.app",
+  messagingSenderId: "947885197929",
+  appId: "1:947885197929:web:f1d1e77654495877a5b0f8",
+  measurementId: "G-NYZXL0V3H2"
 };
 
 // Initialize Firebase
